03454558ef4e1f55ea8d39cdeb5b86d3d09c5c02
Deepak <deepakpatel53536@gmail.com>
2024-08-21 17:26:45 +0530

    deploying in vercel

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use(
 );
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/employee", require("./routes/employeeRoutes"));

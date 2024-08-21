a54b471e272b20c0f863642001a9bfe193f89110
Deepak <deepakpatel53536@gmail.com>
2024-08-21 21:00:04 +0530

    update vite config file

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware to parse JSON requests
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: "https://mern-manage-ment.vercel.app/", // Your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
